1b9bf7501e4f554d49cd2923449c48763aeba3a1
Ikraanaish <Ikraanaish@icloud.com>
2022-10-14 12:42:06 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
  * main - Prints all single digit numbers of base 10 starting from 0,
  * only using putchar and without char variables
  *
- * return: Always 0(Success)
+ * Return: Always 0(Success)
  */
 int main(void)
 {
