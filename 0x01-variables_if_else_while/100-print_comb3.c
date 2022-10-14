4591393ef4fdb7221eafc67854a2419a800549f7
Ikraanaish <Ikraanaish@icloud.com>
2022-10-14 13:08:53 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - Print combinations of two digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+  int n1;
+  int n2;
+
+  for (n1 = 0; n1 <= 9; n1++)
+    {
+      for (n2 = n1 + 1; n2 <= 9; n2++)
+	{
+	  putchar(n1 + '0');
+	  putchar(n2 + '0');
+
+	  if (n1 < 8)
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
+	}
+    }
+  putchar('\n');
+
+  return (0);
+}
