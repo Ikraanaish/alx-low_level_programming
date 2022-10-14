cf5fbea357933db4f40ec9c14a5569540402c20a
Ikraanaish <Ikraanaish@icloud.com>
2022-10-14 11:59:44 -0700

    question 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:Always 0(success)
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c!='q')
+			putcher("c");
+	}
+	putcher('\n');
+
+	Return (0);
+}
