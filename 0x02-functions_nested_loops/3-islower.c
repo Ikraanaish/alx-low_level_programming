5dfff19c41469887670e7d83c635b1c795ab2e62
Ikraanaish <Ikraanaish@icloud.com>
2022-10-18 08:07:23 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase
+ *
+ * Return 1 if llowercase .
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
