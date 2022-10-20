fe11c664aeb94ba059ed259e370af878c0d297a8
Ikraanaish <Ikraanaish@icloud.com>
2022-10-20 14:20:29 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include"main.h"
 /**
- * int _isupper(int c) - checks uppercase character
- *
- * Return: 1 if uppercase; 0 if otherwise
+ * _isupper - A function that checks for uppercase character.
+ * @c: An input character
+ * Return: 1 if c is uppercase or 0 otherwise
  */
 int _isupper(int c)
 {
