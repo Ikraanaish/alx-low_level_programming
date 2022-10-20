fdfcd4506e099dd40e2a7f9bbb25da4f96d04103
Ikraanaish <Ikraanaish@icloud.com>
2022-10-20 15:53:08 -0700

    more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - a ffunction that prints from 0 to 14
+ * 10 times
+ * followed by a new line
+ *
+ * Return: Always 0(Success)
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar('i');
+		}
+		_putchar('\n');
+	}
+}
