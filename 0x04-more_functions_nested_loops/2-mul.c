05cfc2fcec8a4b1c4dc2a02992a68316ab64c52a
Ikraanaish <Ikraanaish@icloud.com>
2022-10-20 15:13:46 -0700

    print

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- * mul - A function that multiplies a and b.
  *
+ * mul - A function that multiplies a and b.
+ * @a - int 
+ *@b - int
  * Return: Always 0
  */
 int mul(int a, int b)
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints digits
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	char i[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	_putchar('i');
+}
