bd0be15ae4ba2d811de2305b2812aad0e9df09cb
Ikraanaish <Ikraanaish@icloud.com>
2022-10-20 14:09:16 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * int _isupper(int c) - checks uppercase character
+ *
+ * Return: 1 if uppercase; 0 if otherwise
+ */
+int _isupper(int c)
+{
+	char i;
+
+	if(i >= 'A' && i <= 'Z')
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
+
