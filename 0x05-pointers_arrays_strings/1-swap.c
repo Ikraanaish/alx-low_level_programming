b78ba8515a520f785482eada66dfdb8309e7d546
Ikraanaish <Ikraanaish@icloud.com>
2022-10-24 08:33:13 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int -a function that swaps two integers
+ * @a: a pointer
+ * @b: another pointer
+ *
+ * Return: always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int i = 0;
+
+	*a = i;
+	*a = *b;
+	*b = i;
+}
