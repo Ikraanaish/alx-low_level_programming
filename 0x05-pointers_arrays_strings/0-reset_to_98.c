9399ec62c02ddb216b38323a071aae027e76fa3e
Ikraanaish <Ikraanaish@icloud.com>
2022-10-24 08:02:14 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a pointer.
+ *@n: pointer to an int
+ *
+ * Return: always 0.
+ */
+void reset_to_98(int *n)
+{
+	int i = 0;
+	*n = &i;
+
+	*n = 98;
+
+	{
+		_putchar(*\n);
+	}
+}
