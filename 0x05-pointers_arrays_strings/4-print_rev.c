f6c447e420bcc037180de904e14603d9b1e29f55
Ikraanaish <Ikraanaish@icloud.com>
2022-10-24 10:40:31 -0700

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - function that prints string in reverse
+ *
+ * @s: a string
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	if (*s != '\0')
+	{
+	print_rev(s + 1);
+	_putchar(*s);
+	}
+}
