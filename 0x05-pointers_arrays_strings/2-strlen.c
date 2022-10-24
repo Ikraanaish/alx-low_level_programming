694bba09e1534113af607b55f91555bbd866e543
Ikraanaish <Ikraanaish@icloud.com>
2022-10-24 09:31:12 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ * @s: pointer to a string
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = *s;
+
+	return (len);
+}
