192f9ea7ce9dd1fb132c3ac5a4ee497449a1a5b9
Ikraanaish <Ikraanaish@icloud.com>
2022-11-02 13:34:13 -0700

    power.0

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - func for x power y
+ * @x: an int
+ * @y: an int
+ * Return: answer.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recurion(x, y - 1));
+	}
+}
