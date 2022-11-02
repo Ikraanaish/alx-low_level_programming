56b74158e8808250e3e8f3b99300f0528afdd222
Ikraanaish <Ikraanaish@icloud.com>
2022-11-02 11:32:00 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion -function to print a string
+ * @s: a string
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
