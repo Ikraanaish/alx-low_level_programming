bc4db93350d7f8cf5959a24d2e9fcfe9b553d250
Ikraanaish <Ikraanaish@icloud.com>
2022-11-02 14:09:09 -0700

    sqrt.3

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,28 @@
 #include "main.h"
+
 /**
- * _sqrt_recursion - to print the sqrt
- * @n: int
- * Return: sqrt
+ * _sqrt_recursion - main funct
+ * @n: int n
+ * Return: int
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt_recursion(n, 1));
+	return (_sqrt(n, 1));
 }
 
 /**
- * _sqrt - sqrt_recursion
- * @n: int
- * @m: int
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
  * Return: sqrt
  */
-int _sqrt(int n, int m)
+int _sqrt(int n, int i)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if ((m * m) > n)
-	{
+	if ((i * i) > n)
 		return (-1);
-	}
-	else
-	{
-		return (_sqrt(n, m + 1));
-	}
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
