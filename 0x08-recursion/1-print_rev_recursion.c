a9988d8ccd3ad1843fa87bc40fe72bf3e3eccaea
Ikraanaish <Ikraanaish@icloud.com>
2022-11-02 11:54:46 -0700

    reverse.0

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "maiin.h"
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: a string to char
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
+	}
+}
